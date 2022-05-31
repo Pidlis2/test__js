e9838a7f6ac41009b2e4033e7e31f5c608b5af0b
Oleksandr Pidlisnyak <78503747+Pidlis2@users.noreply.github.com>
2022-05-31 19:56:44 +0300

    12

---
+++ test.js
@@ -0,0 +1,16 @@
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (checkPassword = null) { // Change this line
+    message =  'Canceled by user!';
+  } else if (checkPassword = ADMIN_PASSWORD) { // Change this line
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+
+  return message;
+}
+console.log(checkPassword('egfefef'));
+
